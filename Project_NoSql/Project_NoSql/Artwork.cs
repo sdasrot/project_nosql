c8868fab676ca84f48224df190da70d65405fac3
Weydert Michael <michael.weydert@devinci.fr>
2015-11-29 19:21:27 +0100

    Classe Artwork

---
+++ Project_NoSql/Project_NoSql/Artwork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_NoSql
+{
+    class Artwork
+    {
+        public int AcquisitionYear { get; set; }
+        public string Classification { get; set; }
+        public string Dimmension { get; set; }
+        public string Title { get; set;  }
+        public string ThumbnailUrl { get; set; }
+    }
+}
